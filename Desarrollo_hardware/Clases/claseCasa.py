17799ecf47e5f6be554aaaf8eface784af814ade
SpeitK <Felipe@Felipes-iMac.home>
2017-11-04 19:03:36 -0300

    indentation fixes

--- Desarrollo_hardware/Clases/claseCasa.py
+++ Desarrollo_hardware/Clases/claseCasa.py
@@ -6,22 +6,22 @@ from threading import Thread
 class casa(object):
 
     def __init__(self):
- 		self.habitaciones = []
+        self.habitaciones = []
 
- 	def addRoom(self, nombre)
- 	# 'nombre' corresponde al nombre o numeracion designada a la habitacion (arbitrario)
- 		self.habitaciones.append(habitacion(nombre))
+    def addRoom(self, nombre):
+        self.habitaciones.append(habitacion(nombre))
+        # 'nombre' corresponde al nombre o numeracion designada a la habitacion (arbitrario)
 
- 	def showRooms(self)
- 		for i in self.habitaciones
- 			print (i.nombre)
+    def showRooms(self):
+        for i in self.habitaciones :
+            print (i.nombre)
 
- 	def showModulesInRooms(self)
- 		for i in self.habitaciones
- 			if i.cortinas!=None :
- 				x = 'Cortinas  '
- 			elif i.luces!=None :
- 				x = x + 'Luces'
- 			else
- 				x = 'Vacia'
- 			print (i.nombre + ' ' + x)
+    def showModulesInRooms(self):
+        for i in self.habitaciones:
+            if i.cortinas!=None :
+                x = 'Cortinas  '
+            elif i.luces!=None :
+                x = x + 'Luces'
+            else:
+                x = 'Vacia'
+            print (i.nombre + ' ' + x)
