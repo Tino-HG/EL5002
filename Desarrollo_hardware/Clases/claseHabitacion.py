c123218e58eabf6090fb41e4acc4b0f7a0c00f0a
SpeitK <Felipe@Felipes-iMac.home>
2017-11-04 19:05:49 -0300

    indentation fixes

--- Desarrollo_hardware/Clases/claseHabitacion.py
+++ Desarrollo_hardware/Clases/claseHabitacion.py
@@ -6,19 +6,19 @@ from threading import Thread
 class habitacion(Thread):
 
     def __init__(self, nombreHabitacion):
- 		Thread.__init__(self)
- 		self.nombre = nombreHabitacion
- 		self.cortinas = None
- 		self.luces = None
+        Thread.__init__(self)
+        self.nombre = nombreHabitacion
+        self.cortinas = None
+        self.luces = None
 
 
- 	def addModules(self, tipoModulo):
- 		if tipoModulo=='Cortinas':
- 			self.cortinas = moduloCortinas()
- 		elif tipoModulo=='Luces':
- 			self.luces = moduloCortinas()
- 		else
- 			print ('%s Error en tipo de modulo en habitacion %d . Ingresar Cortinas o Luces' % self.nombre)
+    def addModules(self, tipoModulo):
+        if tipoModulo=='Cortinas':
+            self.cortinas = moduloCortinas()
+        elif tipoModulo=='Luces':
+            self.luces = moduloCortinas()
+        else:
+            print ('%s Error en tipo de modulo en habitacion %d . Ingresar Cortinas o Luces' % self.nombre)
 
     def run(self):
     	#Activo ambos modulos(?)
